c0281f274f890f8cb0c5cc2d35eeed5e2af93cba
Gabe Johnson <gajo2539@colorado.edu>
2024-09-24 13:43:28 -0600

    Buncha updates for week 4 and 5

---
+++ slide_sources/code.js
@@ -0,0 +1,37 @@
+let count = 0;
+let limit = 10;
+
+while (count < limit) {
+    print("Iteration: " + count);
+    count = count + 1;
+}
+
+let limit = 10;
+for (let count = 0; count < limit; count++) {
+    print("Iteration: " + count);
+}
+
+let flip = 'tales';
+do {
+    flip = flipACoin();
+} while (flip !== 'tales');
+
+let numRows = 4;
+let numCols = 8;
+for (let i = 0; i < numRows; i++) {
+    for (let j = 0; j < numCols; j++) {
+        print("Row", i, "Col", j);
+    }
+}
+
+function glurb(a, b) {
+    if (a > 2 * b) {
+        console.log("A is big");
+    } else {
+        console.log("A isn't that big");
+    }
+    if (a > b) {
+        console.log("(bigger than b)")
+    }
+    console.log(a * b);
+}
